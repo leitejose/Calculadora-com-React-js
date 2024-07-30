e9e82ec715c41893ff323ec5bdb9ff419b6590f8
José Leite <joseleite688@gmail.com>
2024-07-30 23:02:11 +0100

    Commit inicial

---
+++ Styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'; 
+
+export const ButtonContainer = styled.button`
+padding: 20px;
+border: 1px solid #cdcdcd;
+background-color: #00aaf0;
+color: white;
+font-size: 24px;
+font-weight: 700;
+flex: 1;
+&:hover{
+    background-color: #0077cc;
+    cursor: pointer;
+}
+
+
+`
